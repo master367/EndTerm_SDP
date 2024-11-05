53702086079bba10cd6a5ee9d261afff75d1792b
master367 <adiko7634@gmail.com>
2024-11-05 16:33:22 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@ public class Main {
     public static void main(String[] args) {
         CartController cartController = new CartController();
 
+        CartCommand addItemCommand1 = new AddItemCommand(cartController.getCart(), "Laptop");
+        cartController.executeCommand(addItemCommand1);
 
-        CartCommand addItem1 = new AddItemCommand(cartController.getCart(), "Laptop");
-        CartCommand addItem2 = new AddItemCommand(cartController.getCart(), "Phone");
-        CartCommand removeItem = new RemoveItemCommand(cartController.getCart(), "Phone");
+        CartCommand addItemCommand2 = new AddItemCommand(cartController.getCart(), "Smartphone");
+        cartController.executeCommand(addItemCommand2);
 
+        System.out.println("Current items in the cart: " + cartController.getCart().getItems());
 
-        cartController.executeCommand(addItem1);
-        cartController.executeCommand(addItem2);
-        cartController.executeCommand(removeItem);
+        CartCommand removeItemCommand = new RemoveItemCommand(cartController.getCart(), "Laptop");
+        cartController.executeCommand(removeItemCommand);
+
+        System.out.println("Current items in the cart: " + cartController.getCart().getItems());
+
+        CartCommand removeNonExistentItemCommand = new RemoveItemCommand(cartController.getCart(), "Tablet");
+        cartController.executeCommand(removeNonExistentItemCommand);
+
+        cartController.getCart().clearCart();
+        System.out.println("Final items in the cart: " + cartController.getCart().getItems());
     }
 }
